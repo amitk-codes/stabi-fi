a2f94fe31d3308151bfd046535bb822436a10d0e
Amit Kumar <amitkcodes@gmail.com>
2025-02-24 21:25:58 +0530

    created utility functions for withdrawing the collateral and burning the tokens

--- programs/stabi_fi/src/instructions/mod.rs
+++ programs/stabi_fi/src/instructions/mod.rs
@@ -1,7 +1,10 @@
 pub mod initialize_config;
 pub mod deposit_and_mint;
+pub mod withdraw;
 pub mod utils;
+
 
 pub use initialize_config::*;
 pub use deposit_and_mint::*;
+pub use withdraw::*;
 pub use utils::*;
